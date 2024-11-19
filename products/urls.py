56f41e7871af4ad7f6e390b0a75a89fab64b09c0
Radchenko Mukola <RadchenkoMukola@gmail.com>
2024-11-19 16:50:03 +0200

    lab2 added tests

--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views  # Імпортуємо наші функції з views.py
 
 urlpatterns = [
-    path('products/<int:productId>/', views.get_product),  # Додаємо маршрут для ендпоінта
+    path('products/<int:productId>/', views.get_product, name='product_detail'),  # Додаємо маршрут для ендпоінта
 ]
